419a0d9bc4216930130348e0eb2398f81b54b7fe
krixen-dev <krixen.dev@gmail.com>
2023-12-01 03:48:16 -0300

    up

---
+++ callbacks/callbacks.js
@@ -0,0 +1,35 @@
+const botonEjemplo1 = document.querySelector(".button-ejemplo1");
+
+botonEjemplo1.addEventListener("click", function(){
+    return alert("hiciste click")
+})
+
+const textoSecreto = document.querySelector(".texto-secreto");
+
+const botonMensajeSecreto = document.querySelector(".boton-ver-mensaje");
+
+function cambiarMensaje() {
+    textoSecreto.textContent = "Este es el mensaje: SUSCRIBETE a mi canal, no seas rata"
+}
+
+botonMensajeSecreto.addEventListener("click", cambiarMensaje);
+
+const numeroOculto = document.querySelector(".input-numero");
+
+function verificarCoincidencia(e) {
+    if (parseInt(e.target.value) === 1) {
+        console.log("ganas");
+    }else{
+        console.log("pierdes");
+    }
+}
+
+numeroOculto.addEventListener("keydown", (e) => {
+    if (e.key === 'Enter') {
+        console.log("verificando...");
+        setTimeout(function () {
+            verificarCoincidencia(e);
+        }, 3000)
+    }
+})
+
