ad282c4f62ea7a7585ef77ec2caabc6cf1a720f0
krixen-dev <krixen.dev@gmail.com>
2023-12-15 22:11:44 -0300

    up

---
+++ destructuracion/destructuracion.js
@@ -0,0 +1,57 @@
+//con ARRAYS
+const number = [10, 20 , 30];
+
+//ejemplo sin destructuración
+const getNumber = number[0];
+console.log(getNumber);
+
+const getNumber2 = number[2];
+console.log(getNumber2);
+
+//ejemplo con destructuración
+
+const [ten, twenty, thirty] = number;
+console.log(ten);
+console.log(thirty);
+
+//Con OBJETOS
+
+const persona = {
+    nombre : 'cachito',
+    apellido : "fontana",
+    edad : 30
+}
+
+//sin destructuración
+
+const getName = persona.nombre;
+console.log(getName);
+
+//con destructuración
+
+const {nombre, apellido, edad} = persona;
+console.log(nombre);
+
+//CON FUNCIÓN
+const person = { name: 'John', age: 30, city: 'New York' };
+const getData = ({name, city})=>{
+    console.log(name);
+    console.log(city);
+}
+
+getData(person);
+
+
+
+
+
+
+
+
+// function printPersonDetails({ name, city }) {
+//     console.log(name);
+//     console.log(city);
+//   }
+  
+//   printPersonDetails(person);
+
